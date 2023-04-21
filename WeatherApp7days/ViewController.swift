389652a094bff5a49f266192e373f59b8ec17ec6
Carrington <Manyuchi>
2023-04-21 11:15:05 +0200

    List of things we gonna need to achieve  this project.

--- WeatherApp7days/ViewController.swift
+++ WeatherApp7days/ViewController.swift
@@ -3,9 +3,20 @@
 //  WeatherApp7days
 //
 //  Created by Carrington Tafadzwa Manyuchi on 2023/04/21.
-//
 
+//****
+//****
+//**************************************************
+// *** List of things we gonna need in this project ****
+// 1. Location - to get us to know users area
+// 2. TableView - To show the weather list of the upcoming days
+// 3. Custom cell: Collection View - Horizontal table which shows the hourly forecast of the current day
+// 4. API / Request to get the data
+//****
+//****
+//**************************************************
 import UIKit
+
 
 class ViewController: UIViewController {
 
