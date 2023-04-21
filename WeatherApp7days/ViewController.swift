fd961583af19f2a6df78bed1ae2092d89faec4d0
Carrington <Manyuchi>
2023-04-21 11:01:13 +0200

    Initial Commit

---
+++ WeatherApp7days/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp7days
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2023/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
