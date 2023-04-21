7fb85561fc585f575c22b87c92cbeaa899298a51
Carrington <Manyuchi>
2023-04-21 11:38:45 +0200

    Implemented basic table functionality

--- WeatherApp7days/ViewController.swift
+++ WeatherApp7days/ViewController.swift
@@ -18,13 +18,32 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var  table: UITableView!
+    
+    var models = [Weather]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        table.delegate = self
+        table.dataSource = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
+
+
+struct Weather{
+    
+}
